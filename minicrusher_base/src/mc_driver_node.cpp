5c8b93ab03c77d4af30d472fe7b69bb7f1f9ad2c
Eric Perko <wisesage5001@gmail.com>
2012-02-08 15:54:32 -0500

    Added an initial driver node... doesn't compile yet

---
+++ minicrusher_base/src/mc_driver_node.cpp
@@ -0,0 +1,52 @@
+#include <ros/ros.h>
+#include <minicrusher_base/mc_base.h>
+#include <boost/asio.hpp>
+#include <geometry_msgs/Twist.h>
+
+void cmd_vel_callback(const geometry_msgs::Twist& twist)
+{
+  minicrusher_base::command_packet_t cmd_packet;
+  int16_t lefts = floor(twist.linear.x * 1000);
+  int16_t rights = floor(twist.linear.x * 1000);
+
+  cmd_packet.enable = 1;
+  for(uint8_t i = 0; i < 6; i++)
+  {
+    if((i % 2) == 1)
+    {
+      cmd_packet.wheel_vels_mmps[i] = rights;
+    }
+    else
+    {
+      cmd_packet.wheel_vels_mmps[i] = lefts;
+    }
+  }
+
+
+
+}
+
+int main(int argc, char* argv[])
+{
+
+  ros::init(argc, argv, "minicrusher_driver_node");
+  ros::NodeHandle nh;
+  ros::NodeHandle priv_nh("~");
+
+  std::string port;
+  int baud_rate;
+
+  priv_nh.param("port", port, std::string("/dev/ttyUSB0"));
+  priv_nh.param("baud_rate", baud_rate, 115200);
+
+  boost::asio::io_service io;
+
+  try{
+    minicrusher_base::MCBase mc(port, baud_rate, io);
+    ros::Subscriber twist_sub = nh.subscribe<geometry_msgs::Twist>("cmd_vel", 1, cmd_vel_callback);
+  }
+  catch (boost::system::system_error ex) {
+    ROS_ERROR("Error instantiating MCBase. Are you sure you have the correct port and baud rate? Error was %s", ex.what());
+    return -1;
+  }
+}
